2c91ae4dfacf0cc8c00674731f37e2352ded66e5
Amrit Acharya <acharyaamrit47@gmail.com>
2025-12-07 18:24:20 +0545

    controller implemented for handling client req

--- apps/auth/src/auth.controller.ts
+++ apps/auth/src/auth.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
-@Controller()
+@Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Get()
-  getHello(): string {
-    return this.authService.getHello();
+  @Get('nonce')
+  getNounce(@Query('address') address:string){
+    return this.authService.sendNounce(address);
   }
+
+  @Post('verify')
+  verify(@Body() body:{message:string, signature:string}){
+    return this.authService.verify(body);
+  }
+  
 }
--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -3,6 +3,12 @@ import { AuthModule } from './auth.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AuthModule);
-  await app.listen(process.env.port ?? 3000);
+  app.enableCors({
+    origin: true,  
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+  });
+  await app.listen(process.env.port ?? 4000);
 }
 bootstrap();
